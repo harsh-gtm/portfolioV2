0bd0ca97f1a9ed6218308158b4ee245166507e76
harsh <harsh@disent.com>
2026-05-28 16:58:57 -0400

    changed blog content

--- src/app/blog/data.js
+++ src/app/blog/data.js
@@ -1,20 +1,18 @@
 export const content = [
   {
-    title: "Credit Risk Analysis",
-    description: "A streamlit app with ML model trained to analyis credit risk",
+    title: "AI/Machine Learning ",
+    description:
+      "A full roadmap of me learning AI. From learning the algorithms to fully building a transformer based model from scratch. Learning about RAG and supervised vs unsupervised learning. And more...",
     image: "/green.png",
   },
   {
-    title: "Machine Learning",
+    title: "Web Development",
     description:
-      "A full roadmap of me learning AI. From learning the algorithms to fullt building a transformer based model from scratch. Learning about RAG and supervised vs unsupervised learning. And more...",
+      "Expiramenting with React and NextJS while learning GSAP and Motion. Expanding my skills to be expiramental and modern.",
   },
   {
-    title: "title",
-    description: "description",
-  },
-  {
-    title: "title",
-    description: "description",
+    title: "Quant Development",
+    description:
+      "A roadmap of me learning and using statistics with finance to learn more about the quantitative finance world. Learning about HFT, FPGAs, simulating trades with a custom c++ orderbook to handle transactions, and learning about low-latency programming.",
   },
 ];
