7003b9ffde8d8dd6b5a1d7de37638c3d3a220253
harsh <harsh@disent.com>
2026-06-09 18:37:11 -0400

    Added new blurb to use for transition for next section

--- src/app/main/sections/initiatives/initatives.jsx
+++ src/app/main/sections/initiatives/initatives.jsx
@@ -3,12 +3,21 @@ import "./styles.css";
 
 const Initatives = () => {
   return (
-    <div className="next-content">
-      <h1>
-        With my interest in high frequency systems and reaching ultra low
-        latency. everything I build is built on the principle on making things
-        fast and efficeint
-      </h1>
+    <div>
+      <div className="first-blurb">
+        <h1>
+          With my interest in high frequency systems and reaching ultra low
+          latency. everything I build is built on the principle on making things
+          fast and efficeint
+        </h1>
+      </div>
+
+      <div className="second-blurb">
+        <h1>
+          My goal is to be able to bring factual, uncensored information, and
+          rarely talked about events by these 2 projects*
+        </h1>
+      </div>
     </div>
   );
 };
