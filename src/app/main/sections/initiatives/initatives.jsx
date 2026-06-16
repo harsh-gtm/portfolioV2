155199b9c075938466ce5f4f97d6f93f7b1f840b
harsh-gtm <hgautam887@gmail.com>
2026-06-16 14:31:20 -0400

    fixed initative text animation

--- src/app/main/sections/initiatives/initatives.jsx
+++ src/app/main/sections/initiatives/initatives.jsx
@@ -8,6 +8,8 @@ const Initatives = () => {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
+    gsap.set(".text", { opacity: 0 });
+
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: ".first-blurb",
@@ -19,7 +21,7 @@ const Initatives = () => {
       },
     });
 
-    tl.to({}, { duration: 0.5 });
+    tl.to({}, { duration: 0.3 });
 
     tl.set(".rect", {
       clipPath: "polygon(0% 50%, 100% 50%, 100% 50%, 0% 50%)",
@@ -27,20 +29,29 @@ const Initatives = () => {
 
     tl.to(".rect", {
       clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      duration: 0.2,
       ease: "none",
     });
 
     tl.to(".rect", {
-      scaleX: 60,
+      scaleX: 200,
       transformOrigin: "center center",
+      duration: 0.37,
       ease: "none",
-    });
+    }).to(
+      ".text",
+      {
+        opacity: 1,
+        duration: 0.2,
+      },
+      "-=0.3",
+    );
   }, []);
 
   return (
     <>
       <div className="first-blurb">
-        <h1 className="text">
+        <h1>
           My interests range from CS, machine learning, Quantitative finance,
           and data science
         </h1>
@@ -48,6 +59,9 @@ const Initatives = () => {
 
       <div className="rect-container">
         <div className="rect"></div>
+        <div className="text">
+          <h1>And my projects target to help people.</h1>
+        </div>
       </div>
     </>
   );
